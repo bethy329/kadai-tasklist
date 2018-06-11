1bd0ca14567cb9438762556bfb14327965975fe9
yuto.inada <adgjm_50@icloud.com>
2018-06-11 13:15:42 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
-  get 'toppages/index'
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
 
   root to: 'toppages#index'
 
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
+
   resources :tasks
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
 end
