8457ee3ef8cbe4fbca22c1d5379b4d1408bdec87
yuto.inada <adgjm_50@icloud.com>
2018-06-14 16:00:12 +0000

    post

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -3,7 +3,7 @@ class ToppagesController < ApplicationController
     if logged_in?
       @user = current_user
       @task = current_user.tasks.build
-      
+      @tasks = Task.all.page(params[:page]).per(3)
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,18 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-  
+ 
+ 
+ 
   delete 'tasks/destroy'
 
   root to: 'toppages#index'
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
-  get 'logout', to: 'sessions#destroy'
+  delete 'logout', to: 'sessions#destroy'
 
   resources :tasks
   get 'signup', to: 'users#new'
   
-  resources :users, only: [:index, :show, :new, :create,]
+  resources :users, only: [:index, :show, :new, :create]
   resources :tasks, only: [:create, :destroy]
 end
