864a89515a6019cd506d045aba03ebedba78fcb1
yuto.inada <adgjm_50@icloud.com>
2018-06-14 06:05:05 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,13 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
-
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:edit, :show, :update, :destroy]
   def index
-    @tasks = Task.all.page(params[:page]).per(3)
+    redirect_to root_url
+    #@tasks = Task.all.page(params[:page]).per(3)
   end
   
   def show
+    @task = Task.find(params[:id])
   end
   
   def new
@@ -13,12 +15,12 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(task_params)
-
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
       redirect_to @task
     else
+      #@tasks = current_user.feed_tasks.order('created_at DESC').page(params[:page])
       flash.now[:danger] = 'Task が投稿されませんでした'
       render :new
     end
@@ -40,18 +42,21 @@ class TasksController < ApplicationController
   def destroy
     
     @task.destroy
-
     flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url
+    redirect_back(fallback_location: root_path)
   end
   
   private
-  
-  def set_task
-    @task= Task.find(params[:id])
-  end
+
   
   def task_params
     params.require(:task).permit(:content, :status)
   end
+  
+   def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+   end
 end
--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,8 +1,9 @@
 class ToppagesController < ApplicationController
   def index
-    
+    if logged_in?
+      @user = current_user
+      @task = current_user.tasks.build
       
-     
-   
+    end
   end
 end
